5ee1796cca17ca8e4b9bd85747988258f42a0e13
Richard Bray <rich.bray@orva.studio>
2025-11-29 21:53:55 +0000

    Initial Commit

---
+++ NoClap/NoClapApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoClapApp.swift
+//  NoClap
+//
+//  Created by Richard Oliver Bray on 29/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct NoClapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
